6cc680f40f4cb01ba4c72cfdf9ebc8ac6b70038e
Li Zhao <iloahz@gmail.com>
2014-03-25 00:58:36 +0800

    add code for max flow using dinic

---
+++ lib/graph/flow/maxflow/poj1273.cpp
@@ -0,0 +1,118 @@
+///https://github.com/acmicpc
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int MAXN = 205;
+const int MAXM = 205;
+const int MAXNODE = MAXN;
+const int MAXE = MAXM * 2;
+const int INF = 1 << 29;
+
+int n, m;
+struct Edge{
+    int u, v, nxt;
+    int cap, flow;
+}   e[MAXE];
+int totalEdge, pnt[MAXNODE];
+int ss, st;
+
+void addEdge(int x, int y, int z){
+    int i = totalEdge++;
+    e[i].u = x;
+    e[i].v = y;
+    e[i].cap = z;
+    e[i].flow = 0;
+    e[i].nxt = pnt[x];
+    pnt[x] = i;
+}
+
+void addBi(int x, int y, int z){
+    addEdge(x, y, z);
+    addEdge(y, x, 0);
+}
+
+int dis[MAXNODE];
+int cur[MAXNODE];
+int q[MAXNODE];
+bool bfs(){
+    for (int i = 0;i <= st;i++) dis[i] = -1;
+    for (int i = 0;i <= st;i++) cur[i] = pnt[i];
+    int l = 0, r = 0;
+    dis[ss] = 0;
+    q[r++] = ss;
+    while (l < r){
+        int cx = q[l++];
+        for (int i = pnt[cx];i >= 0;i = e[i].nxt){
+            int nx = e[i].v;
+            if (dis[nx] < 0 && e[i].flow < e[i].cap){
+                dis[nx] = dis[cx] + 1;
+                if (nx == st) return true;
+                q[r++] = nx;
+            }
+        }
+    }
+    return false;
+}
+
+int dfs(int x, int y){
+    if (x == st) return y;
+    for (int &i = cur[x];i >= 0;i = e[i].nxt){
+        int nx = e[i].v;
+        if (dis[nx] == dis[x] + 1 && e[i].flow < e[i].cap){
+            int r = dfs(nx, min(y, e[i].cap - e[i].flow));
+            if (r){
+                e[i].flow += r;
+                e[i ^ 1].flow -= r;
+                return r;
+            }
+        }
+    }
+    return 0;
+}
+
+int maxflow(){
+    int tot = 0;
+    while (bfs()){
+        while (1){
+            int cur = dfs(ss, INF);
+            if (cur == 0) break;
+            tot += cur;
+        }
+    }
+    return tot;
+}
+
+void init(){
+    if (scanf("%d%d", &m, &n) == EOF) exit(0);
+    ss = 1; st = n;
+    totalEdge = 0;
+    for (int i = 0;i <= st;i++) pnt[i] = -1;
+    for (int i = 0;i < m;i++){
+        int x, y, z;
+        scanf("%d%d%d", &x, &y, &z);
+        addBi(x, y, z);
+    }
+}
+
+void work(){
+    printf("%d\n", maxflow());
+}
+
+int main(){
+#ifdef LATTE
+    freopen("poj1273.in", "r", stdin);
+#endif
+    while (1){
+        init();
+        work();
+    }
+    return 0;
+}
